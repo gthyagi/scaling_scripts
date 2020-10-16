086e1a054f26e6b0602472d693734562ea184103
thyagi <thyagarajulu.gollapalli@monash.edu>
2020-10-17 01:42:03 +1100

    initial modification to scaling scripts. 1. added annulus .py script 2. modified parameters for weaking scaling

--- weak_params.sh
+++ weak_params.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-export JOBS="1 2 4 6 8 10"
-export UW_NAME="bknight2d_test_take1"
+export JOBS="1 2 4 8"
+export UW_NAME="thyagi2d_test_take1"
 export UW_ENABLE_IO="0"
 
-export WALLTIME="00:10:00"
-export ACCOUNT="m18"
+export WALLTIME="00:01:00"
+export ACCOUNT="n69"
 
 export WEAK_SCALING_BASE=128
 export UW_ORDER=1
-export UW_SOL_TOLERANCE=1e-6
+export UW_SOL_TOLERANCE=1e-3
 export UW_PENALTY=1e-3   # set to negative value to disable penalty 
-export UW_SCRIPT="bknight_2d.py"
+export UW_SCRIPT="1024_512_sum_660.py"
 export UW_DIM=2
